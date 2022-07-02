53e0d62d74d66f5c554e428d7a4dc7d500bef907
aditya <priyadarshiaditya46@gmail.com>
2022-07-03 01:07:26 +0530

    updated

--- methods.js
+++ methods.js
@@ -5,13 +5,6 @@ const app = express();
 //middleware : whatever data comes from front-end, convert that to json
 app.use(express.json());
 
-const userRoute = express.Router();
-const authRouter = express.Router();
-
-//base router
-app.use('/users',userRoute);
-app.use('/auth',authRouter);
-
 let users = [
     {
         'id' : 1,
@@ -27,75 +20,27 @@ let users = [
     }
 ]
 
-userRoute
-.route('/')
-.get(getUser)
-.post(postUser)
-.patch(updateUser)
-.delete(deleteUser)
 
-userRoute
-.route('/:id')
-.get(getUserById)
+app.get('/',(req,res) => {
+    // res.send('/');
+    res.sendFile('D:\VSCode\Backend\views\index.html');
+})
+app.get('/views/about.html',(req,res) => {
+    // res.send('Hello');
+    res.sendFile('./views/about.html',{root: __dirname})
+})
 
+//redirect
 
-authRouter
-.route('/signup')
-.get(getSignUp)
-.post(postSignUp)
+app.get('/about-us',(req,res) => {
+    res.redirect('/about')
+})
 
-function getUser(req,res){
-    res.send(users);
-}
+// 404 page
 
-function postUser(req,res){
-    console.log(req.body)
-    users = req.body;
-    res.json({
-        message: "Data received successfully",
-        user : req.body
-    })
-}
-
-function updateUser(req,res){
-    console.log(req.body);
-
-    let dataToBeUpdated = req.body;
-    users.push(dataToBeUpdated);
-    // for(key in dataToBeUpdated) {
-    //     users[key]  = dataToBeUpdated[key]
-    // }
-
-    res.json({
-        message: "Data updated successfully"
-    })
-}
-
-function deleteUser(req,res){
-    users.shift();
-  res.json({
-    message: "Data deleted successfully"
-  })
-}
-
-function getUserById(req,res){
-    res.send('user id received ');
-    console.log(req.params.id);
-    console.log(req.params)
-}
-
-function getSignUp(req,res){
-    res.sendFile('/public/index.html',{root:__dirname})
-}
-
-function postSignUp(req,res){
-   let objReceived = req.body;
-   console.log(objReceived);
-    res.json({
-        message: 'signed up successfully',
-        data: objReceived
-    })
-}
+app.use((req,res) => {
+    res.status(404).sendFile('./views/404.html',{root: __dirname})
+})
 
 
 app.listen(3000)
